9a87d7c46b1f5c7a35981c6ff37b154492fd70f0
Samuel Wellander <samwellander@gmail.com>
2017-07-28 22:38:08 -0700

    Import common components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import firebase from 'firebase';
+import firebase from 'firebase'; 
 import reducers from './reducers';
 
 class App extends Component {
